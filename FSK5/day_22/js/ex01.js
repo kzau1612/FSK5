59d30f183fc0ee4659d1b88da8aad1594ae01513
kzau1612 <ngohoangkim2002@gmail.com>
2024-02-17 21:36:44 +0700

    bai 1

---
+++ FSK5/day_22/js/ex01.js
@@ -0,0 +1,12 @@
+var arrA = [1, 4, 3, 2];
+var arrB = [5, 2, 6, 7, 1];
+
+var result = [];
+for (var i = 0; i < arrA.length; i++) {
+  var findnum = arrB.find((number) => {
+    if (number === arrA[i]) {
+      result.push(number);
+    }
+  });
+}
+console.log(result);
