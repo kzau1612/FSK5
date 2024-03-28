34d0f67bbc612909971f77ee0ab2ff9ea27c95c6
kzau1612 <ngohoangkim2002@gmail.com>
2024-03-28 21:40:20 +0700

    update button

--- FSK5/day_33/js/scripts.js
+++ FSK5/day_33/js/scripts.js
@@ -1,6 +1,7 @@
 var fileSelect = document.querySelector(".file-select");
 var fileList = document.querySelector(".file-list");
 var filename = document.querySelector(".file-name");
+var selectedText;
 
 filename.value = "Untitle";
 
@@ -11,4 +12,26 @@ fileSelect.addEventListener("click", function () {
   } else {
     fileSelect.style.backgroundColor = "#0d6efd";
   }
+});
+
+var content = document.querySelector("#content");
+
+window.addEventListener("mouseup", () => {
+  var selectedText = window.getSelection().toString().trim();
+  console.log(selectedText);
+});
+
+var boldBtn = document.querySelector("#bold-btn");
+boldBtn.addEventListener("click", function () {
+  document.execCommand("bold");
+});
+
+var italicBtn = document.querySelector("#italic-btn");
+italicBtn.addEventListener("click", function () {
+  document.execCommand("italic");
+});
+
+var underlineBtn = document.querySelector("#underline-btn");
+underlineBtn.addEventListener("click", function () {
+  document.execCommand("underline");
 });
