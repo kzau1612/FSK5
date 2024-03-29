c97808c68fb6728b7c39f2d64b4528aa5a64801c
kzau1612 <ngohoangkim2002@gmail.com>
2024-03-29 10:35:38 +0700

    ok

--- FSK5/day_33/js/scripts.js
+++ FSK5/day_33/js/scripts.js
@@ -1,9 +1,14 @@
 var fileSelect = document.querySelector(".file-select");
 var fileList = document.querySelector(".file-list");
-var filename = document.querySelector(".file-name");
+var input = document.querySelector(".file-name__input");
+var fileName;
 var selectedText;
+var newBtn = fileList.children[0];
+var txtBtn = fileList.children[1];
+var pdfBtn = fileList.children[2];
 
-filename.value = "Untitle";
+input.value = "Untitle";
+fileName = input.value;
 
 fileSelect.addEventListener("click", function () {
   fileList.classList.toggle("hidden");
@@ -15,11 +20,12 @@ fileSelect.addEventListener("click", function () {
 });
 
 var content = document.querySelector("#content");
+var contentText;
+var contentHtml;
 
-window.addEventListener("mouseup", () => {
-  var selectedText = window.getSelection().toString().trim();
-  console.log(selectedText);
-});
+// window.addEventListener("mouseup", () => {
+//   var selectedText = window.getSelection().toString().trim();
+// });
 
 var boldBtn = document.querySelector("#bold-btn");
 boldBtn.addEventListener("click", function () {
@@ -46,8 +52,38 @@ colorBtn.addEventListener("change", function () {
 var row = document.querySelector(".row");
 var letterNum = row.children[0];
 var wordNum = row.children[1];
+
 content.addEventListener("input", function () {
   var text = content.innerText.trim();
   letterNum.innerText = "Số kí tự: " + text.length;
   wordNum.innerText = "Số từ: " + text.split(" ").length;
+  contentText = content.innerText;
+  contentHtml = content.innerHTML;
+});
+
+input.addEventListener("input", function () {
+  fileName = input.value;
+});
+
+newBtn.addEventListener("click", function () {
+  content.innerText = "";
+  fileList.classList.toggle("hidden");
+});
+
+txtBtn.addEventListener("click", function () {
+  fileList.classList.toggle("hidden");
+  var blob = new Blob([contentText]);
+  var blobUrl = URL.createObjectURL(blob);
+  var a = document.createElement("a");
+  a.href = blobUrl;
+  a.download = `${fileName}.txt`;
+  a.click();
+});
+
+pdfBtn.addEventListener("click", function () {
+  fileList.classList.toggle("hidden");
+  var option = {
+    filename: `${fileName}.pdf`,
+  };
+  html2pdf(contentHtml, option);
 });
