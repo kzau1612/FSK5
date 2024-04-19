9a9578168847b60b49c66dabc9faa8f269f8b4de
kzau1612 <ngohoangkim2002@gmail.com>
2024-04-19 14:21:12 +0700

    them countdown

--- FSK5/day_39/js/scripts.js
+++ FSK5/day_39/js/scripts.js
@@ -8,47 +8,60 @@ let score = document.querySelector(".score");
 let question = document.querySelector(".question");
 const answerWrapper = document.querySelector(".answers-wrapper");
 let answers = document.querySelectorAll(".answer");
+let countdownNum = +document.querySelector(".count-down").innerText;
 
 startBtn.addEventListener("click", () => {
-  root.innerHTML = `      <div class="top">
-  <div class="top-left">
-    <span><span class="question-num">1</span>/<span class="total-question">8</span></span>
-    <div class="streak-bar">
-      <div class="streak-line-1"></div>
-      <div class="streak-line-2"></div>
-      <div class="streak-progress"></div>
+  const countdown = setInterval(() => {
+    if (countdownNum < 1) {
+      clearInterval(countdown);
+      root.innerHTML = `      <div class="top">
+      <div class="top-left">
+        <span><span class="question-num">1</span>/<span class="total-question">8</span></span>
+        <div class="streak-bar">
+          <div class="streak-line-1"></div>
+          <div class="streak-line-2"></div>
+          <div class="streak-progress"></div>
+        </div>
+        <span class="bonus">+100</span>
+      </div>
+      <div class="top-right">
+        <span>Score:<span class="score">0</span></span>
+      </div>
     </div>
-    <span class="bonus">+100</span>
-  </div>
-  <div class="top-right">
-    <span>Score:<span class="score">0</span></span>
-  </div>
-</div>
-<div class="question-wrapper">
-  <p class="question">okosakdoasodasodasodka</p>
-</div>
+    <div class="question-wrapper">
+      <p class="question">okosakdoasodasodasodka</p>
+    </div>
+  
+    <div class="answers-wrapper">
+      <div class="answer">
+        <p class="answer-text">Answer 1</p>
+      </div>
+      <div class="answer">
+        <p class="answer-text">Answer 2</p>
+      </div>
+      <div class="answer">
+        <p class="answer-text">Answer 3</p>
+      </div>
+      <div class="answer">
+        <p class="answer-text">Answer 4</p>
+      </div>
+    </div>
+    <div class="correct hidden">
+      <p>Correct!!</p>
+    </div>
+    <div class="incorrect hidden">
+      <p>Incorrect!!</p>
+    </div>
+    </div>`;
+    } else {
+      root.innerHTML = `<p class="count-down">${countdownNum}</p>`;
+      +countdownNum--;
+    }
+  }, 1000);
 
-<div class="answers-wrapper">
-  <div class="answer">
-    <p class="answer-text">Answer 1</p>
-  </div>
-  <div class="answer">
-    <p class="answer-text">Answer 2</p>
-  </div>
-  <div class="answer">
-    <p class="answer-text">Answer 3</p>
-  </div>
-  <div class="answer">
-    <p class="answer-text">Answer 4</p>
-  </div>
-</div>
-<div class="correct hidden">
-  <p>Correct!!</p>
-</div>
-<div class="incorrect hidden">
-  <p>Incorrect!!</p>
-</div>
-</div>`;
+  // setTimeout(() => {
+
+  // }, 3000);
 });
 
 answers.forEach((answer) => {
