1ae00706058728b8b259a4844e58b2f27cee5bb3
kzau1612 <ngohoangkim2002@gmail.com>
2024-05-28 17:11:14 +0700

    khoi tao

---
+++ FSK5/day_45/vite-project/src/App.jsx
@@ -0,0 +1,109 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h2 className="title">Welcome to Shop!</h2>
+      <ul className="list">
+        <li className="item">
+          <img
+            src="https://images.unsplash.com/photo-1716847214582-d5979adbf300?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="image"
+            className="item-img"
+          />
+          <h3 className="item-name">Trang Sức Mitsubishi</h3>
+          <div className="row">
+            <span className="item-price">$435134</span>
+            <button className="item-btn">Add to cart!</button>
+          </div>
+        </li>
+        <li className="item">
+          <img
+            src="https://images.unsplash.com/photo-1716847214582-d5979adbf300?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="image"
+            className="item-img"
+          />
+          <h3 className="item-name">Trang Sức Mitsubishi</h3>
+          <div className="row">
+            <span className="item-price">$435134</span>
+            <button className="item-btn">Add to cart!</button>
+          </div>
+        </li>
+        <li className="item">
+          <img
+            src="https://images.unsplash.com/photo-1716847214582-d5979adbf300?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="image"
+            className="item-img"
+          />
+          <h3 className="item-name">Trang Sức Mitsubishi</h3>
+          <div className="row">
+            <span className="item-price">$435134</span>
+            <button className="item-btn">Add to cart!</button>
+          </div>
+        </li>
+        <li className="item">
+          <img
+            src="https://images.unsplash.com/photo-1716847214582-d5979adbf300?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="image"
+            className="item-img"
+          />
+          <h3 className="item-name">Trang Sức Mitsubishi</h3>
+          <div className="row">
+            <span className="item-price">$435134</span>
+            <button className="item-btn">Add to cart!</button>
+          </div>
+        </li>
+        <li className="item">
+          <img
+            src="https://images.unsplash.com/photo-1716847214582-d5979adbf300?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="image"
+            className="item-img"
+          />
+          <h3 className="item-name">Trang Sức Mitsubishi</h3>
+          <div className="row">
+            <span className="item-price">$435134</span>
+            <button className="item-btn">Add to cart!</button>
+          </div>
+        </li>
+        <li className="item">
+          <img
+            src="https://images.unsplash.com/photo-1716847214582-d5979adbf300?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="image"
+            className="item-img"
+          />
+          <h3 className="item-name">Trang Sức Mitsubishi</h3>
+          <div className="row">
+            <span className="item-price">$435134</span>
+            <button className="item-btn">Add to cart!</button>
+          </div>
+        </li>
+        <li className="item">
+          <img
+            src="https://images.unsplash.com/photo-1716847214582-d5979adbf300?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="image"
+            className="item-img"
+          />
+          <h3 className="item-name">Trang Sức Mitsubishi</h3>
+          <div className="row">
+            <span className="item-price">$435134</span>
+            <button className="item-btn">Add to cart!</button>
+          </div>
+        </li>
+        <li className="item">
+          <img
+            src="https://images.unsplash.com/photo-1716847214582-d5979adbf300?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="image"
+            className="item-img"
+          />
+          <h3 className="item-name">Trang Sức Mitsubishi</h3>
+          <div className="row">
+            <span className="item-price">$435134</span>
+            <button className="item-btn">Add to cart!</button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
